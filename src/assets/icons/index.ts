c584afe8a29a09c3d517684080be58bbd5fe0bc2
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-08-06 11:57:56 +0100

    chore: update global styles, change app title, and add new assets.

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,9 @@
+// serve icons from here...
+
+import logo_icon from "./logo.svg";
+import caret_down_icon from "./caret-down.svg";
+import app_grid_icon from "./app-grid.svg";
+import search_icon from "./search.svg";
+import more_icon from "./three-dots.svg";
+
+export { logo_icon, caret_down_icon, app_grid_icon, search_icon, more_icon };
---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+import image_1 from "./image1.png";
+import image_2 from "./image2.png";
+import image_3 from "./image3.png";
+import image_4 from "./image4.png";
+
+export { image_1, image_2, image_3, image_4 };
